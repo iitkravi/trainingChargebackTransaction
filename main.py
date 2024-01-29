dc6f1c34c3684efbc5134d683fc8deef17676e40
iitkravi <ravi.cse.iitk@gmail.com>
2024-01-30 01:40:33 +0530

    use label encoder.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # This is a sample Python script.
-
+import numpy
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 # Import the libraries
 import pandas as pd
+from sklearn.feature_extraction import FeatureHasher
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.tree import plot_tree
@@ -14,14 +16,26 @@ import matplotlib.pyplot as plt
 import pickle
 
 
+class FlattenForEach:
+    pass
+
+
 def train_model():
     # Load and preprocess the data
     data = pd.read_csv("data.csv")  # Replace with your CSV file name
     data = data.fillna(0)  # Fill missing values with 0
-    data = pd.get_dummies(data)  # Encode categorical variables
+    # data = pd.get_dummies(data)  # Encode categorical variables
     X = data.drop("target", axis=1)  # Separate the features from the target
     y = data["target"]  # Assign the target variable
-
+    #X = X["merchant_name"]
+    encoder = LabelEncoder()
+    h = FeatureHasher(n_features=7, input_type='string')
+    X = X[['merchant_name', 'merchant_id', 'merchant_email', 'merchant_phone', 'customer_name', 'customer_card_number',
+           'customer_billing_address', 'city', 'zip_code', 'order_date', 'order_amount', 'order_currency',
+           'order_description', 'payment_authorization_code', 'payment_settlement_date', 'latitude', 'longitude',
+           'ip_address']].apply(encoder.fit_transform)
+    numpy.save('classes.npy', encoder.classes_, allow_pickle=True)
+    y = y.astype("int")
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
