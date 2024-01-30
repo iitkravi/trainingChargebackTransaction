963d7d63a54caa4c530b87aefa3705421f96016e
iitkravi <ravi.cse.iitk@gmail.com>
2024-01-30 21:40:53 +0530

    update preprocessing logic.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import hashlib
+
 import numpy
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -20,21 +22,73 @@ class FlattenForEach:
     pass
 
 
+def update_r(row):
+    h = hashlib.sha256()
+    h.update(row['merchant_name'].encode())
+    row['merchant_name'] = int(h.hexdigest(), 16) % (10 ** 8)
+
+    # h.update(row['merchant_id'].encode())
+    # row['merchant_id'] = int(h.hexdigest(), 16)
+
+    h.update(row['merchant_email'].encode())
+    row['merchant_email'] = int(h.hexdigest(), 16) % (10 ** 8)
+
+    h.update(row['merchant_phone'].encode())
+    row['merchant_phone'] = int(h.hexdigest(), 16) % (10 ** 8)
+
+    h.update(row['customer_name'].encode())
+    row['customer_name'] = int(h.hexdigest(), 16) % (10 ** 8)
+
+    # h.update(row['customer_card_number'].encode())
+    # row['customer_card_number'] = int(h.hexdigest(), 16)
+
+    h.update(row['customer_billing_address'].encode())
+    row['customer_billing_address'] = int(h.hexdigest(), 16) % (10 ** 8)
+
+    h.update(row['city'].encode())
+    row['city'] = int(h.hexdigest(), 16) % (10 ** 8)
+
+    h.update(row['zip_code'].encode())
+    row['zip_code'] = int(h.hexdigest(), 16) % (10 ** 8)
+
+    h.update(row['order_currency'].encode())
+    row['order_currency'] = int(h.hexdigest(), 16) % (10 ** 8)
+
+    h.update(row['order_description'].encode())
+    row['order_description'] = int(h.hexdigest(), 16) % (10 ** 8)
+
+    h.update(row['payment_authorization_code'].encode())
+    row['payment_authorization_code'] = int(h.hexdigest(), 16) % (10 ** 8)
+
+    # h.update(row['latitude'].encode())
+    # row['latitude'] = int(h.hexdigest(), 16)
+    #
+    # h.update(row['longitude'].encode())
+    # row['longitude'] = int(h.hexdigest(), 16)
+
+    h.update(row['ip_address'].encode())
+    row['ip_address'] = int(h.hexdigest(), 16) % (10 ** 8)
+    return row
+
 def train_model():
     # Load and preprocess the data
     data = pd.read_csv("data.csv")  # Replace with your CSV file name
     data = data.fillna(0)  # Fill missing values with 0
     # data = pd.get_dummies(data)  # Encode categorical variables
     X = data.drop("target", axis=1)  # Separate the features from the target
+    X = data.drop("order_date", axis=1)
+    X = data.drop("payment_settlement_date", axis=1)
+    # X = data.drop("latitude", axis=1)
+    # X = data.drop("longitude", axis=1)
     y = data["target"]  # Assign the target variable
-    #X = X["merchant_name"]
-    encoder = LabelEncoder()
-    h = FeatureHasher(n_features=7, input_type='string')
+    # X = X["merchant_name"]
+    # encoder = LabelEncoder()
+    # h = FeatureHasher(n_features=7, input_type='string')
     X = X[['merchant_name', 'merchant_id', 'merchant_email', 'merchant_phone', 'customer_name', 'customer_card_number',
-           'customer_billing_address', 'city', 'zip_code', 'order_date', 'order_amount', 'order_currency',
-           'order_description', 'payment_authorization_code', 'payment_settlement_date', 'latitude', 'longitude',
-           'ip_address']].apply(encoder.fit_transform)
-    numpy.save('classes.npy', encoder.classes_, allow_pickle=True)
+           'customer_billing_address', 'city', 'zip_code', 'order_amount', 'order_currency', 'latitude', 'longitude',
+           'order_description', 'payment_authorization_code',
+           'ip_address']].apply(update_r, axis=1)
+    # numpy.save('classes.npy', encoder.classes_, allow_pickle=True)
     y = y.astype("int")
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
